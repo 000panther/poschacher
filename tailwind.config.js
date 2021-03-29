08916ee29beaa30ae750b5ed64c646934f96d106
Paul Weber <paul@apollo.ai>
2021-03-29 20:32:57 +0000

    Add some style changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@ module.exports = {
   purge: [ "./src/_includes/**/*.njk", "./src/*.html" ],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: theme => ({
+       'top-image': "url('/brueckevonarselectronica.jpg')",
+      })
+    }
   },
   variants: {
     extend: {},
